8fdf1aeb712813406a9c5dbc5b975d8eeb78d770
mickamy <t.mikami1992@gmail.com>
2019-11-01 01:55:17 +0900

    Initial commit

---
+++ BetterSlider/src/BetterSlider.h
@@ -0,0 +1,19 @@
+//
+//  BetterSlider.h
+//  BetterSlider
+//
+//  Created by Tetsuro Mikami on 2019/11/01.
+//  Copyright © 2019 Tetsuro Mikami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetterSlider.
+FOUNDATION_EXPORT double BetterSliderVersionNumber;
+
+//! Project version string for BetterSlider.
+FOUNDATION_EXPORT const unsigned char BetterSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterSlider/PublicHeader.h>
+
+
---
+++ Example/src/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Tetsuro Mikami on 2019/11/01.
+//  Copyright © 2019 Tetsuro Mikami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
